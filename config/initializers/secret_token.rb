86ef9a5659ff6ff76e35f56c7bbdb4270d555f36
Camilo Barraza <cbarraza11@gmail.com>
2014-06-13 22:10:15 -0500

    first commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Template2::Application.config.secret_key_base = '02af10bf3702b842f41181b3f6f1eec312f3ad0099e2f72bb719e72ae1b70b6a6f675bc19288300821237543a327687d525ad6c7a608893a4b825660f84e7eea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Template2::Application.config.session_store :cookie_store, key: '_template2_session'
