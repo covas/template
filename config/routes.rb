92dc1c33e685b3e85d27ebad9ebd2ef3d8c47e85
Camilo Barraza <cbarraza11@gmail.com>
2014-06-13 22:39:07 -0500

    neuvo controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Template2::Application.routes.draw do
   get 'test' => 'test#test'
+  get 'nuevo' => 'nuevo#nuevo'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
